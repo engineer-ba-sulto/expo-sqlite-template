13134a37c3f6052bfaf635ce0c69260c1cbf60bd
Hiroki Chiyoda <contact@arafipro.com>
2025-10-31 16:16:24 +0900

    Indexコンポーネントにマイグレーション機能を追加
    - `useMigrations`を使用してデータベースのマイグレーションの成功・失敗を管理する機能を実装しました。
    - マイグレーションの進行状況やエラーメッセージを表示するためのUIを追加しました。
    - 取得したデータの表示形式を改善しました。

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,38 @@
 import db from "@/drizzle/db";
+import migrations from "@/drizzle/migrations/migrations";
 import { todoTable } from "@/drizzle/schema/todoSchema";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const { success, error } = useMigrations(db, migrations);
   const { data } = useLiveQuery(db.select().from(todoTable));
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
-      <Text>{JSON.stringify(data)}</Text>
+      <Text className="text-2xl text-gray-500">
+        Data:{JSON.stringify(data)}
+      </Text>
     </View>
   );
 }
