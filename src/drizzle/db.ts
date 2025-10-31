fe86ac753c2456afd2afbf2c3c197641d3cbf2ec
Hiroki Chiyoda <contact@arafipro.com>
2025-10-31 15:02:41 +0900

    データベース接続の設定を更新
    - `src/drizzle/db.ts`ファイルで、`openDatabaseSync`関数に`{ enableChangeListener: true }`オプションを追加し、データベースの変更リスナーを有効にしました。

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
 import * as userSchema from "./schema/todoSchema";
 
-const expoDb = openDatabaseSync("db.db");
+const expoDb = openDatabaseSync("db.db", { enableChangeListener: true });
 const db = drizzle(expoDb, { schema: userSchema });
 
 export default db;
