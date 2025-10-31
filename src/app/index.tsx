a086e88ef0fbd28d108c7d6cbaf5db2309f70f64
Hiroki Chiyoda <contact@arafipro.com>
2025-10-31 15:03:52 +0900

    Indexコンポーネントにデータベースからのリアルタイムクエリを追加
    - `useLiveQuery`を使用して`todoTable`からデータを取得し、表示する機能を実装しました。
    - 取得したデータをJSON形式で表示するための`Text`コンポーネントを追加しました。

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,16 @@
+import db from "@/drizzle/db";
+import { todoTable } from "@/drizzle/schema/todoSchema";
+import { useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const { data } = useLiveQuery(db.select().from(todoTable));
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
+      <Text>{JSON.stringify(data)}</Text>
     </View>
   );
 }
