d2ee6b9f733724f9b9cd70320359c8a47b13486a
Hiroki Chiyoda <contact@arafipro.com>
2025-10-31 14:17:12 +0900

    Drizzle ORMの設定用にdrizzle.config.tsを追加
    - Drizzle ORM用の新しい設定ファイルを導入。SQLiteをダイアレクトとして指定し、スキーマとマイグレーションのパスを定義。
    - この設定は、アプリケーションのドライバーとしてExpoをサポートします。

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./src/drizzle/schema/*",
+  out: "./src/drizzle/migrations",
+  driver: "expo",
+});
