3c03df31e63b49b025f5ce7fcbb6a6fbded302bb
Hiroki Chiyoda <contact@arafipro.com>
2025-11-01 10:05:54 +0900

    既存のTodo更新用にUpdateTodoInput型を追加
    - `todo.ts`に`UpdateTodoInput`型を追加。新たに導入された`updateTodoSchema`から推論。
    - これにより、アプリケーション内の既存Todoを更新する際の型安全性が向上。

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,4 +1,5 @@
-import { createTodoSchema } from "@/zod/todo.schema";
+import { createTodoSchema, updateTodoSchema } from "@/zod/todo.schema";
 import { z } from "zod";
 
 export type CreateTodoInput = z.infer<typeof createTodoSchema>;
+export type UpdateTodoInput = z.infer<typeof updateTodoSchema>;
